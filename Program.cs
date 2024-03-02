1adac6cd39a02cbe420377e12bea53b403c806b4
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-03-02 19:52:53 +0300

    Add cors policy

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 using System.Text.Json;
 
+var AllowSpecificOrigins = "_AllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowSpecificOrigins,
+    policy  =>
+    {
+        policy.WithOrigins("http://localhost:3000", "http://testtaskfe:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -17,6 +29,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(AllowSpecificOrigins);
 
 
 app.MapGet("/projects/all", () =>
